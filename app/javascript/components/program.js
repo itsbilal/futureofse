3fa0d5319c93746cf26ba8072f3dbdd8230d4d0e
Dave Pagurek <dave@davepagurek.com>
2018-01-20 18:36:54 -0800

    Add diff view, fix vertical align

--- app/javascript/components/program.js
+++ app/javascript/components/program.js
@@ -1,17 +1,37 @@
-
 import React from 'react'
 
 import Term from './term'
 
 class Program extends React.Component {
   render() {
+    if (this.props.diff) {
+      return this.renderDiff();
+    } else {
+      return this.renderSingle();
+    }
+  }
+
+  renderSingle() {
     return (
-      <div className={`program program-${this.props.vertical ? 'vertical' : 'scroll'}`}>
+      <div className="program program-scroll">
         { this.props.program.terms.map((term) => (
           <Term key={term} courses={this.props.courses} editable={this.props.editable} term={this.props.program[term]} name={term} />
         )) }
       </div>
-    )
+    );
+  }
+
+  renderDiff() {
+    return (
+      <div className="program program-vertical">
+        { this.props.program.terms.map((term) => (
+          <div className="row" key={`row-${term}`}>
+            <Term key={`orig-${term}`} courses={this.props.courses} editable={this.props.editable} term={this.props.program[term]} name={`Proposed ${term}`} />
+            <Term key={`diff-${term}`} courses={this.props.courses} editable="false" term={this.props.diff[term]} name={`Current ${term}`} />
+          </div>
+        )) }
+      </div>
+    );
   }
 }
 
