bd9939ed90a1eb8088ae6dac75fb396d059f8d82
Bilal Akhtar <me@itsbilal.com>
2018-01-24 19:39:16 -0800

    Add Sentry support in prod

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module Futureofse
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    Raven.configure do |config|
+      config.dsn = 'https://6f1157e57dfa4a6fb08f6e32af442038:4f67312906924f78af2affc5018e8270@sentry.io/277244'
+    end
   end
 end
