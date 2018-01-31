07146893c20aac72b6c3a93b644ebb8238980427
Bilal Akhtar <me@itsbilal.com>
2018-01-30 20:58:45 -0800

    Update extract.sql

--- extract.sql
+++ extract.sql
@@ -4,8 +4,8 @@ WITH term_courses AS (SELECT v2.id AS voter_id, pdc.term, string_agg(pdc.course,
   INNER JOIN program_design_courses pdc ON pdc.program_design_id = pd1.id
   GROUP BY v2.id, pdc.term
 )
-SELECT v.created_at, v.cls, v.name, v.email, v.current, v.reason, q1.answer, q2.answer, q3.answer, q4.answer, p1.comment, p2.comment,
- t1a.courses, t1b.courses, t2a.courses, t2b.courses, t3a.courses, t3b.courses, t4a.courses, t4b.courses 
+SELECT v.created_at, v.cls, v.name, v.email, v.current, q1.answer AS answer1, q2.answer AS answer2, q3.answer AS answer3, q4.answer AS answer4, p1.comment AS oldcomment, p2.comment AS newcomment, pd.comment AS designercomment,
+ t1a.courses AS c1a, t1b.courses AS c1b, t2a.courses AS c2a, t2b.courses AS c2b, t3a.courses AS c3a, t3b.courses AS c3b, t4a.courses AS c4a, t4b.courses AS c4b 
  FROM voters v
   INNER JOIN stage_responses s1 ON s1.voter_id = v.id
   INNER JOIN questions q1 ON q1.stage_response_id = s1.id AND q1.question = 0
@@ -19,6 +19,8 @@ SELECT v.created_at, v.cls, v.name, v.email, v.current, v.reason, q1.answer, q2.
   INNER JOIN program_views p1 ON p1.stage_response_id = s5.id
   INNER JOIN stage_responses s6 ON s6.voter_id = v.id AND s6.stage = 3
   INNER JOIN program_views p2 ON p2.stage_response_id = s6.id
+  INNER JOIN stage_responses s7 ON s7.voter_id = v.id AND s7.stage = 4
+  INNER JOIN program_designs pd ON pd.stage_response_id = s7.id
   INNER JOIN term_courses t1a ON t1a.voter_id = v.id AND t1a.term = '1A'
   INNER JOIN term_courses t1b ON t1b.voter_id = v.id AND t1b.term = '1B'
   INNER JOIN term_courses t2a ON t2a.voter_id = v.id AND t2a.term = '2A'
